87c1d8e35abfbd79857dec657c92e2c5a02a7277
Ahmed Haider <ahmed.haider@get10up.com>
2024-09-15 14:47:16 +0500

    feat: navbar margin adjustment

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
       (scrolled)
         ? "fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100"
         : "fixed mx-auto top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100"}>
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-0 laptop:mx-auto px-4 py-2">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
